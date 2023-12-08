ec94300a753f63631e1837e9b0fc621e99bf5ee7
Rohittz <shinderohitd19@gmail.com>
2023-12-08 18:03:53 +0530

    Add new files

---
+++ app.js
@@ -0,0 +1,96 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true;  //playerX, player0
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const resetGame = () =>{
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {
+            box.innerText = "O";
+            box.classList.add("playerO");
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            box.classList.add("playerX");
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const showWinner = (winner) =>{
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    let isDraw = true;
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val == pos2Val && pos2Val == pos3Val) {
+                showWinner(pos1Val);
+                return;
+            }
+        }
+    }
+
+    //check for a draw
+
+    for(let box of boxes){
+        if(box.innerText == ""){
+            isDraw = false;
+            break;
+        }
+    }
+
+    if(isDraw){
+        msg.innerText = "It's a draw!"
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
+
